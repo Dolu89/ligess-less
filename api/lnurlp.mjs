f76da567f2401ab46d6867e8b5a4d651977ee997
Dolu <doludev@protonmail.com>
2022-10-26 19:44:43 +0200

    feat: add an optionnal lnproxy

--- api/lnurlp.mjs
+++ api/lnurlp.mjs
@@ -1,5 +1,6 @@
 import UnaConnect from "una-connect-js/dist/index.js";
 import crypto from "crypto";
+import fetch from "node-fetch";
 
 export default async function handler(request, response) {
   const _username = process.env.NAME;
@@ -10,18 +11,31 @@ export default async function handler(request, response) {
     ["text/identifier", _identifier],
     ["text/plain", `Satoshis to ${_identifier}`],
   ];
+  const minSendable = process.env.MIN_SENDABLE || 1000000;
+  const maxSendable = process.env.MAX_SENDABLE || 1000000000000;
 
   if (!request.query.amount) {
     return response.status(200).json({
       status: "OK",
       callback: _lnurlpUrl,
       tag: "payRequest",
-      maxSendable: 100000000,
-      minSendable: 1000,
+      maxSendable,
+      minSendable,
       metadata: JSON.stringify(_metadata),
       commentAllowed: 0,
     });
   }
+
+  let amountMsats = request.query.amount;
+  if (amountMsats < minSendable) {
+    return response.status(200).json({
+      status: "ERROR",
+      reason:
+        "Amount should be in milliSats and can't be under minSendable params",
+    });
+  }
+
+  let bolt11 = "";
 
   const unaConnect = new UnaConnect(
     process.env.NOSTR_PRIVATE_KEY,
@@ -29,21 +43,44 @@ export default async function handler(request, response) {
   );
 
   const metadata = JSON.stringify(_metadata);
-  const description = crypto
+  const descriptionHash = crypto
     .createHash("sha256")
     .update(metadata)
     .digest("hex");
 
-  const result = await unaConnect.createInvoice(
-    request.query.amount,
-    description
-  );
+  let routingMsats = 0;
+  if (process.env.LNPROXY_URL) {
+    const lnproxyBaseMsat = process.env.LNPROXY_BASE_FEE || 1000;
+    const lnproxyPpmMsat = process.env.LNPROXY_PPM_FEE || 6000;
+    routingMsats =
+      +lnproxyBaseMsat + (+lnproxyPpmMsat * amountMsats) / 1_000_000;
+    amountMsats = amountMsats - routingMsats;
+  }
+
+  const result = await unaConnect.createInvoice({
+    amountMsats,
+    descriptionHash,
+  });
+
+  bolt11 = result.bolt11;
+
+  if (process.env.LNPROXY_URL) {
+    const res = await fetch(
+      `${process.env.LNPROXY_URL}/api/${bolt11}?routing_msat=${routingMsats}`,
+      {
+        method: "GET",
+      }
+    );
+
+    const invoiceProxy = await res.text();
+    bolt11 = invoiceProxy.replace("\n", "");
+  }
 
   response.status(200).json({
     status: "OK",
     successAction: { tag: "message", message: "Payment received!" },
     routes: [],
-    pr: result.bolt11,
+    pr: bolt11,
     disposable: false,
   });
 }
